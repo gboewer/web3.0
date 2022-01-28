bee523bdbf558ad04d1bc371da43990eb3ce9f9f
s1nglz <boewer.gil@gmail.com>
2022-01-28 18:26:40 +0100

    basic file structure and some design code

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar, Welcome, Footer, Services, Transactions} from './components';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
+
+export default App
